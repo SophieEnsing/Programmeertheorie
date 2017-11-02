de3994e58f60cf365f7ac94d6594709c4c29721a
Sophie Ensing <sophie_ensing@hotmail.com>
2017-11-02 14:10:49 +0100

    Netwerk

---
+++ Network.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+
+G = nx.Graph()
+
+with open('StationsHolland.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        G.add_node(row[0], pos=(float(row[1]), float(row[2])))
+
+pos = {city:(long, lat) for (city, (lat,long) in nx.get_node_attributes(G, 'pos').items()}
+
+with open('ConnectiesHolland.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        G.add_edge(row[0], row[1], weight = int(row[2]))
+
+nx.draw(G, pos, with_labels = True, node_size = 50, font_size = 7)
+plt.show()
