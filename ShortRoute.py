78e1cf77c1e191b8ad26b3f7b7a4973ab3ebfcfa
Rosalie Snijders <snijdersrosalie@hotmail.com>
2017-11-22 14:01:37 +0100

    22/11

--- ShortRoute.py
+++ ShortRoute.py
@@ -43,10 +43,19 @@ def shortroute(station, trajectTijd):
 	i = 0
 
 	if tijd < trajectTijd:
-		route.append(station)		
-		kort = sorted(verbinding[station])[i]
+		route.append(station)	
 
-		try:
+		verbindingen = [ (tijd, eindstation) for tijd, eindstation in verbinding[station] if eindstation not in route ]
+		kritiekeVerbindingen = [ (tijd, eindstation) for tijd, eindstation in verbindingen if eindstation in stationsKritiek ]
+
+		if len(kritiekeVerbindingen) >= 1:
+			kort = sorted(kritiekeVerbindingen)[i]
+		elif len(verbindingen) >= 1:
+			kort = sorted(verbindingen)[i]
+		else:
+			return route, tijd
+
+		'''try:
 			# Ga niet terug naar het voorgaande station
 			while kort[1] == route[-2]:
 				if i == (len(verbinding[station]) - 1):
@@ -54,10 +63,11 @@ def shortroute(station, trajectTijd):
 				kort = sorted(verbinding[station])[i + 1]
 				i += 1
 		except:
-			pass
+			pass'''
 		
 		station = kort[1]
 		tijd += int(kort[0])
+
 		shortroute(station, trajectTijd)
 
 	return route, tijd
@@ -82,34 +92,3 @@ def testLijnvoering(trajectTijd, aantalTrajecten):
 		trajecten.append(traject)
 
 	return trajecten
-
-def scoreLijnvoering(lijnvoering):
-	""" Bereken de score van de lijnvoering
-	Score functie S > S = p*10000 - (t*20 + min/100000)
-	"""
-	verbindingen = []
-	aantalTrajecten = len(lijnvoering)
-	aantalMinuten = 0
-
-	for traject in lijnvoering:
-		# Bijhouden van totale tijd in minuten van de lijnvoering
-		aantalMinuten += list(traject.values())[0][1]
-		traject = list(traject.values())[0][0]
-
-		# Maak van een traject een lijst van verbindingen in dat traject
-		trajectParen = [(traject[i], traject[i+1]) for i in range(0, len(traject)-1 ,1)]
-		# Check voor elke verbinding of een van de stations kritiek is
-		trajectParen2 = [traject for traject in trajectParen if traject[0] in stationsKritiek or traject[1] in stationsKritiek]
-		# Voeg de kritieke verbindingen toe aan een lijst
-		verbindingen += trajectParen2
-
-	# Set van verbindingen om dubbele tuples te voorkomen
-	setTrajectParen = list(set([ tuple(sorted(t)) for t in verbindingen]))
-	percKritiek = len(setTrajectParen) / len(verbindingKritiek)
-
-	S = (percKritiek * 10000) - ((aantalTrajecten * 20) + (aantalMinuten / 100000))
-	return S
-
-# Gemiddelde en hoogste score bij 10,000 keer uitvoeren van de functie
-result = [scoreLijnvoering(testLijnvoering(100, 7)) for i in range(10000000)]
-print("mean:", np.mean(result), "max:", max(result)) 
