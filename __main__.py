49a342cc50176b47801bf4a5a036ce379d1c6a64
Sophie Ensing <sophie_ensing@hotmail.com>
2017-12-05 14:09:44 +0100

    5/12

---
+++ __main__.py
@@ -0,0 +1,10 @@
+from Algoritmes.HillClimber import *
+from Algoritmes.Random import *
+from Functies.ReadData import *
+from Functies.Score import *
+from Functies.ShortKritiek import *
+
+#stations_, stationsKritiek_ = inlezenStations('Data/StationsHolland.csv')
+#verbinding_, verbindingKritiek_ = inlezenVerbindingen('Data/ConnectiesHolland.csv')
+
+print(stations)
