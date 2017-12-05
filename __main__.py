f79d286fb03620b6fc6aa97561fea64e84033f93
Sophie Ensing <sophie_ensing@hotmail.com>
2017-12-05 14:49:03 +0100

    5/12

--- __main__.py
+++ __main__.py
@@ -3,8 +3,4 @@ from Algoritmes.Random import *
 from Functies.ReadData import *
 from Functies.Score import *
 from Functies.ShortKritiek import *
-
-#stations_, stationsKritiek_ = inlezenStations('Data/StationsHolland.csv')
-#verbinding_, verbindingKritiek_ = inlezenVerbindingen('Data/ConnectiesHolland.csv')
-
-print(stations)
+import copy
