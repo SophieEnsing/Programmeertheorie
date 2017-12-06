cc2b0e7e6f9c95e500fc797b10124bf37972ac32
Sophie Ensing <sophie_ensing@hotmail.com>
2017-12-06 15:09:08 +0100

    6/12

--- Algoritmes/SimulatedAnnealing.py
+++ Algoritmes/SimulatedAnnealing.py
@@ -3,148 +3,34 @@ import random
 import numpy as np
 import csv
 import math
-import bigfloat 
 import matplotlib.pyplot as plt 
+from Functies.Score import *
+from Functies.ShortKritiek import *
 
-""" Alle code tot de hillclimber functie zelf is dubbelop met andere bestanden. Het is
-de bedoeling dat deze code later geimporteerd wordt vanuit de andere bestanden.
-"""
-
-# Inlezen van alle data
-stations = []
-stationsKritiek = []
-
-with open('../Data/StationsHolland.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        stations.append(row[0])
-        if row[3] == 'Kritiek':
-        	# Voeg alle kritieke stations toe aan een lijst
-        	stationsKritiek.append(row[0])
-
-verbinding = {}
-verbindingKritiek = []
-
-with open('../Data/ConnectiesHolland.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-    	# Alle verbindingen toevoegen per station in een dictionary
-        verbinding[row[0]] = verbinding.get(row[0], []) + [(float(row[2]), row[1])]
-        verbinding[row[1]] = verbinding.get(row[1], []) + [(float(row[2]), row[0])]
-
-        # Maak een lijst van alle kritieke verbindingen
-        if row[0] in stationsKritiek or row[1] in stationsKritiek:
-        	verbindingKritiek.append((row[0], row[1]))
-
-tijd = 0
-route = []
-
-def shortKritiek(station, trajectTijd):
-	""" Maak een traject vanuit een beginstation, vanuit dit
-	station wordt het korste kritieke pad gekozen
-	Trajecttijd: maximale tijd in minuten voor het traject
-	Station: beginstation van het traject """
-	global tijd
-	global route
-	
-	route.append(station)	
-
-	# Maak lijsten van normale verbindingen en kritieke verbindingen
-	verbindingen = [ (afstand, eindstation) for afstand, eindstation in verbinding[station] 
-						if eindstation not in route ]
-	kritiekeVerbindingen = [ (afstand, eindstation) for afstand, eindstation in 
-							verbindingen if eindstation in stationsKritiek or station in stationsKritiek ]
-
-	# Kies eerst de kortste kritieke verbinding en anders de kortste verbinding
-	if len(kritiekeVerbindingen) >= 1:
-		kort = sorted(kritiekeVerbindingen)[0]
-
-	elif len(verbindingen) >= 1:
-		kort = sorted(verbindingen)[0]
-
-	else:
-		return route, tijd
-
-	if tijd + int(kort[0]) < trajectTijd:
-		station = kort[1]
-		tijd += int(kort[0])
-		shortKritiek(station, trajectTijd)
-
-	return route, tijd
-
-#print(shortKritiek("Leiden Centraal", 50))
-
-def lijnvoering(trajectTijd, beginStations):
-	"""" Maakt een lijnvoering van verschillende trajecten
-	Trajecttijd: maximale tijd in minuten per traject"""
-
-	# Genereer random beginstations
-	trajecten = []
-	global tijd
-	global route
-
-	# Maak voor elk beginstation een traject
-	for stat in beginStations:
-		traject = {}
-		tijd = 0
-		route = []
-		traject[stat] = shortKritiek(stat, trajectTijd)
-		trajecten.append(traject)
-
-	return trajecten
-
-def scoreLijnvoering(lijnvoering):
-	""" Bereken de score van de lijnvoering
-	Score functie S > S = p*10000 - (t*20 + min/100000)
-	"""
-	verbindingen = []
-	aantalTrajecten = len(lijnvoering)
-	aantalMinuten = 0
-
-	for traject in lijnvoering:
-		# Bijhouden van totale tijd in minuten van de lijnvoering
-		aantalMinuten += list(traject.values())[0][1]
-		traject = list(traject.values())[0][0]
-
-		# Maak van een traject een lijst van verbindingen in dat traject
-		trajectParen = [(traject[i], traject[i+1]) for i in range(0, len(traject)-1 ,1)]
-		# Check voor elke verbinding of een van de stations kritiek is
-		trajectParen2 = [traject for traject in trajectParen if traject[0] in stationsKritiek or traject[1] in stationsKritiek]
-		# Voeg de kritieke verbindingen toe aan een lijst
-		verbindingen += trajectParen2
-
-	# Set van verbindingen om dubbele tuples te voorkomen
-	setTrajectParen = list(set([ tuple(sorted(t)) for t in verbindingen]))
-	percKritiek = len(setTrajectParen) / len(verbindingKritiek)
-
-	S = (percKritiek * 10000) - ((aantalTrajecten * 20) + (aantalMinuten / 100000))
-	return S
-
-def grafiek(y_waardes, aantal_interaties):
-	plt.plot(range(0,aantal_interaties), y_waardes)
-	plt.axis([0, aantal_interaties, 0, 10000])
-	plt.show()
+# def grafiek(y_waardes, aantal_interaties):
+# 	plt.plot(range(0,aantal_interaties), y_waardes)
+# 	plt.axis([0, aantal_interaties, 0, 10000])
+# 	plt.show()
 
 def acceptance(old, new, T):
 	a = np.exp((new - old) / T)
 	return a
 
-
-def SimulatedAnnealing(trajectTijd, aantalTrajecten):
+def simulatedAnnealing(trajectTijd, aantalTrajecten, classname):
 	T = 1
 	T_min = 0.0001
 	#alpha = 0.999
 
-	beginStations = random.sample(stations, aantalTrajecten)
+	beginStations = random.sample(classname.stations, aantalTrajecten)
 
 	# Maak een begin state en archief aan
-	currentState = lijnvoering(trajectTijd, beginStations)
-	currentScore = scoreLijnvoering(currentState)
+	currentState = lijnvoering(trajectTijd, beginStations, classname)
+	currentScore = scoreLijnvoering(currentState, classname)
 	archief = [sorted(beginStations)]
 	maxScore = 0
+	bestStations = []
 	
 	# C houdt bij hoevaak achter elkaar er geen betere oplossing is
-	#c = 0
 	aantal_iteraties = 0
 	y_waardes = []
 
@@ -153,14 +39,15 @@ def SimulatedAnnealing(trajectTijd, aantalTrajecten):
 		# Stop als er 100 keer achter elkaar geen betere oplossing is gevonden
 		c = 0
 		y_waardes.append(maxScore)
-		aantal_iteraties += 1
+		#aantal_iteraties += 1
+
 		while c < 100:
 			#aantal_iteraties += 1
 			index = random.randint(0, (aantalTrajecten - 1)) 
-			station = random.choice(stations)
+			station = random.choice(classname.stations)
 
 			while station in beginStations:
-				station = random.choice(stations)
+				station = random.choice(classname.stations)
 
 			# Pas een van de stations aan van de beginstations
 			nieuweStations = beginStations
@@ -170,12 +57,12 @@ def SimulatedAnnealing(trajectTijd, aantalTrajecten):
 			# Check of de combinatie van stations niet in het archief staat
 			if nieuweStations not in archief:
 				archief.append(nieuweStations)
-				newState = lijnvoering(trajectTijd, nieuweStations)
-				newScore = scoreLijnvoering(newState)
+				newState = lijnvoering(trajectTijd, nieuweStations, classname)
+				newScore = scoreLijnvoering(newState, classname)
 
 				# Bereken acceptatiekans
 				a = acceptance(currentScore, newScore, T)
-				print currentScore, newScore, a
+				#print currentScore, newScore, a
 
 				if a > random.random():
 					currentState = newState
@@ -184,12 +71,11 @@ def SimulatedAnnealing(trajectTijd, aantalTrajecten):
 
 					if newScore > maxScore:
 						maxScore = newScore
+						bestStations = nieuweStations
 
 			c += 1
 
-		T = T - 0.001
+		T = T - 0.01
 
-	grafiek(y_waardes, aantal_iteraties)
-
-print(SimulatedAnnealing(120,7))
-
+	#grafiek(y_waardes, aantal_iteraties)
+	return maxScore, nieuweStations
--- __main__.py
+++ __main__.py
@@ -1,3 +1,10 @@
 from Algoritmes.HillClimber import *
 from Algoritmes.Random import *
+from Algoritmes.SimulatedAnnealing import *
 from Functies.ReadData import *
+
+print("1 = Random algoritme")
+print("2 = Hillclimber algoritme")
+print("3 = Simulated Annealing algoritme")
+
+#algoritme = raw_input("Welk algoritme wil je uitvoeren?")
