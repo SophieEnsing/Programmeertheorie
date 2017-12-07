75992c9ec336a218be91d9ad909ce84aec7ce1db
Sophie Ensing <sophie_ensing@hotmail.com>
2017-12-07 13:55:23 +0100

    7/12

--- __main__.py
+++ __main__.py
@@ -27,26 +27,26 @@ programmaKeuze = input("Welk programma wil je uitvoeren? ")
 print("")
 classKeuze = input("Voor alleen Noord- en Zuid-Holland kies 1 en voor heel Nederland kies 2. ")
 
-if programmaKeuze == 1:
-	if classKeuze == 1:
+if programmaKeuze == "1":
+	if classKeuze == "1":
 		print(randomAlgoritme(1000, Holland, 120, 7))
-	elif classKeuze == 2:
+	elif classKeuze == "2":
 		print(randomAlgoritme(1000, Nederland, 180, 20))
 
-elif programmaKeuze == 2:
-	if classKeuze == 1:
+elif programmaKeuze == "2":
+	if classKeuze == "1":
 		print(hillClimber(120, 7, Holland))
-	elif classKeuze == 2:
+	elif classKeuze == "2":
 		print(hillClimber(180, 20, Nederland))
 
-elif programmaKeuze == 3:
-	if classKeuze == 1:
+elif programmaKeuze == "3":
+	if classKeuze == "1":
 		print(simulatedAnnealing(120, 7, Holland))
-	elif classKeuze == 2:
+	elif classKeuze == "2":
 		print(simulatedAnnealing(180, 20, Nederland))
 
-elif programmaKeuze == 4:
-	if classKeuze == 1:
+elif programmaKeuze == "4":
+	if classKeuze == "1":
 		visualisatie('Data/StationsHolland.csv', 'Data/ConnectiesHolland.csv')
-	elif classKeuze == 2:
+	elif classKeuze == "2":
 		visualisatie('Data/StationsNationaal.csv', 'Data/ConnectiesNationaal.csv')
