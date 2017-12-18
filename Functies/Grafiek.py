ef9ad20fe68650a2d3766c1ec7048f7c6461bf02
RosalieSnijders <snijdersrosalie@hotmail.com>
2017-12-18 16:27:24 +0100

    Delete Grafiek.py

--- Functies/Grafiek.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt 
-
-def grafiek(y_waardes, aantal_interaties):
-	plt.plot(range(0,aantal_interaties), y_waardes)
-	plt.axis([0, aantal_interaties, 0, 10000])
-	plt.show()
